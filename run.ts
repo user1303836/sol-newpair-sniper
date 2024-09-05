7ec2fff4153381898d67f3a5c59713a9dc454364
user1303836 <gcvvvvvv@gmail.com>
2024-09-05 11:37:58 -0400

    .

--- run.ts
+++ run.ts
@@ -1,6 +1,8 @@
 import { retrieveEnvVariable } from "./src/pools";
 import { logger } from "./src/logger";
-import { Connection } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
+import { Liquidity, LiquidityPoolKeys } from "@raydium-io/raydium-sdk";
+import { MinimalMarketLayoutV3 } from "./src/tokens";
 
 const network = "mainnet-beta";
 const RPC_ENDPOINT = retrieveEnvVariable("RPC_ENDPOINT", logger);
@@ -9,3 +11,11 @@ const RPC_WEBSOCKET = retrieveEnvVariable("RPC_WEBSOCKET", logger);
 const solanaConnection = new Connection(RPC_ENDPOINT, {
   wsEndpoint: RPC_WEBSOCKET,
 });
+
+export type MinimalTokenAccountData = {
+  mint: PublicKey;
+  address: PublicKey;
+  buyValue?: number;
+  poolKeys?: LiquidityPoolKeys;
+  market?: MinimalMarketLayoutV3;
+};
---
+++ src/tokens.ts
@@ -0,0 +1,5 @@
+import { GetStructureSchema } from "@raydium-io/raydium-sdk";
+
+export type MinimalMarketStateLayoutV3 = typeof MINIMAL_MARKET_STATE_LAYOUT_V3;
+export type MinimalMarketLayoutV3 =
+  GetStructureSchema<MinimalMarketStateLayoutv3>;
